8774b7e21d2b4c096f27373cfc527abc8baefcd6
Bernat Borrás Paronella <alorma@users.noreply.github.com>
2017-05-04 21:18:59 +0200

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,86 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const crypto = require('crypto');
+const secureCompare = require('secure-compare');
+const moment = require('moment');
+
+exports.githubWebhook = functions.https.onRequest((req, res) => {
+  const cipher = 'sha1';
+  const signature = req.headers['x-hub-signature'];
+
+  // TODO: Configure the `github.secret` Google Cloud environment variables.
+  const hmac = crypto.createHmac(cipher, functions.config().github.secret)
+      // The JSON body is automatically parsed by Cloud Functions so we re-stringify it.
+      .update(JSON.stringify(req.body, null, 0))
+      .digest('hex');
+  const expectedSignature = `${cipher}=${hmac}`;
+
+  // Check that the body of the request has been signed with the GitHub Secret.
+  if (secureCompare(signature, expectedSignature)) {
+	processRequest(req, res);
+  } else {
+    console.error('x-hub-signature', signature, 'did not match', expectedSignature);
+    res.status(403).send('Your x-hub-signature\'s bad and you should feel bad!');
+  }
+});
+
+function processRequest(req, res) {
+	var payload = req.body;
+	var review = payload.review;
+	var user = review.user;
+
+	sendResponseToGithub(res, user, review);
+	sendReviewToDatabase(review.state, user, review);
+}
+
+function sendReviewToDatabase(rState, rUser, review) {
+	if (rState !== 'pending') {
+		var date = moment(review.submitted_at);
+		review['user'] = {};
+		review['_links'] = {};
+		review['commit_id'] = {};
+		review['submitted_at'] = {};
+
+		var day   = date.format('DD');
+		var month = date.format('MM');
+		var year  = date.format('YYYY');
+
+		var dayRef = admin.database().ref(year + '-' + month + '-' + day);
+
+		dayRef.child('totalCount').transaction(function(count) {
+			return (count || 0) + 1;
+		});
+
+		var reviewStateRef = dayRef.child(rState);
+
+		reviewStateRef.child('count').transaction(function(count) {
+			return (count || 0) + 1;
+		});
+
+		var userRef = reviewStateRef.child(rUser.login);
+
+		userRef.child('login').set(rUser.login);
+		userRef.child('avatar').set(rUser.avatar_url);
+
+		var reviewRef = userRef.child('reviews').push();
+
+		reviewRef.set(review);
+	}
+}
+
+function sendResponseToGithub(res, user, review) {
+	var reviewResponse = {
+		user: user.login,
+		state: review.state,
+		link: review._links.html.href
+	};
+  	res.send(JSON.stringify(reviewResponse));
+}
+
+
+
+
+
+
